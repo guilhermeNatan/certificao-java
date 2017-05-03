ce646d7d62190bedd05507b00d48b3af622f2e61
guilherme_natan <guilherme.natan.barbosa@gmail.com>
2017-05-02 23:31:09 -0300

    Exemplos de Acesso a váriaveis estáticas e Inicialização de váriaveis dentro de métodos.

---
+++ exemplos/variaveis/AcessoVariavelStatica.java
@@ -0,0 +1,16 @@
+class VariavelStatica {
+      
+ 	 String name; 
+	
+	public static void main (String... args)
+	{
+		// Código não irá compilar pq um método estatico só pode
+		// acessar outros métodos ou váriaveis estaticas 
+		System.out.println(name);
+
+	}
+
+} 
+
+
+
---
+++ exemplos/variaveis/InializacaoVarMetodo.java
@@ -0,0 +1,14 @@
+ class InicialiacacaoErroCompilacao {
+	public static void main(String... args)
+	{
+		// Códico não irá compila pois toda várivel dentro de método tem que ser inicializada:q
+
+		String varNaoInicializada; 
+		char price; 
+
+		System.out.println(varNaoInicializada); 
+		System.out.println(price);
+	}
+	
+}
+
