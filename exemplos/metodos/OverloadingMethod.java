d3ef333b5e3c2519a26848a378b47419c55ac234
guilherme_natan <guilherme.natan.barbosa@gmail.com>
2017-05-03 22:43:55 -0300

    Exemplo de Overloading Method

--- exemplos/metodos/OverloadingMethod.java
+++ exemplos/metodos/OverloadingMethod.java
@@ -1,7 +1,7 @@
 public class OverloadingMethod {
 
 	public static int metodo( int...days)	{return 0;}
-	// sobrecarga com varargs n„o pode ser feita mudando apenas para um array de mesmo tipo
+	// sobrecarga com varargs n√£o pode ser feita mudando apenas para um array de mesmo tipo
 	//public static int metodo( int []days){}
 	
 	public static int metodoDois(int a)	{ return 0;}
@@ -10,10 +10,10 @@ public class OverloadingMethod {
 	//mudando os tipos de parametros 
 	public static int metodoDois(char x, char y)	{return 0;}	
 	public static int metodoDois(int x, char y)	{return 0;}
-	//mudando posiÁ„o de parametros
+	//mudando posi√ß√£o de parametros
 	public static int metodoDois(char x, int y)	{return 0;}
 
-	// N√O IR¡ FUNCIONAR 
+	// N√ÉO IR√Å FUNCIONAR 
 	// mundando somente o retorno
 	//public static char metodoDois(int a)	{}
 	//mudando somente o modificador de acesso 	
